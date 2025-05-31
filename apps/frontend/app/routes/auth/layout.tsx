0f6882aaa1b66fb155f075bcd1c55d9380547aa7
Arthur Andrade <arthurfelandrade@gmail.com>
2025-05-31 01:58:30 -0300

    style(auth): ajusta layout responsivo do card de autenticação
    - Adiciona altura calculada em telas mobile
    
    - Ajusta padding em telas desktop
    
    - Melhora comportamento do scroll

--- apps/frontend/app/routes/auth/layout.tsx
+++ apps/frontend/app/routes/auth/layout.tsx
@@ -14,7 +14,7 @@ export default function AuthLayout() {
       <div className="w-full max-w-md mb-1" >
         <span className="text-2xl font-londrina">{pathMapper[location.pathname as keyof typeof pathMapper]}</span>
       </div>
-      <Card className="w-full max-w-md p-8 flex flex-col gap-8 pt-16 pb-16">
+      <Card className="w-full max-w-md p-8 flex flex-col gap-8 pt-8 pb-8 h-[calc(100vh-80px)] md:pt-16 md:pb-16 md:h-auto overflow-scroll">
         <div className="flex flex-col items-center gap-8">
           <h1 className="text-7xl font-bold text-center">VARIANT LISTS</h1>
         </div>
