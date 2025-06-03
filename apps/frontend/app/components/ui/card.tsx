399a9b51951815c2eea7d59c8221034dca675c25
Arthur Andrade <arthurfelandrade@gmail.com>
2025-06-03 00:38:39 -0300

    feat: Add heading e card-item component (#21)

--- apps/frontend/app/components/ui/card.tsx
+++ apps/frontend/app/components/ui/card.tsx
@@ -1,23 +1,37 @@
 import * as React from "react";
 import { cn } from "~/lib/utils";
 
-interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  footerMessage?: string;
+  wrapperClassName?: React.HTMLAttributes<HTMLDivElement>["className"];
+}
 
 const Card = React.forwardRef<HTMLDivElement, CardProps>(
-  ({ className, children, ...props }, ref) => {
+  ({ className, wrapperClassName, children, footerMessage, ...props }, ref) => {
     return (
-      <div
-        ref={ref}
-        className={cn(
-          "drop-shadow-custom rounded-[16px] border-[4px] border-black bg-white",
-          className,
+      <div className={cn("relative", wrapperClassName)}>
+        <div
+          ref={ref}
+          className={cn(
+            "drop-shadow-custom rounded-[16px] border-[4px] border-black bg-white",
+            footerMessage && "drop-shadow-error",
+            className
+          )}
+          {...props}
+        >
+          {children}
+        </div>
+        {footerMessage && (
+          <p
+            title={footerMessage}
+            className="text-white text-sm absolute -bottom-5 truncate left-0 w-[calc(100%-20px)] translate-x-[16px]"
+          >
+            {footerMessage}
+          </p>
         )}
-        {...props}
-      >
-        {children}
       </div>
     );
-  },
+  }
 );
 
 Card.displayName = "Card";
